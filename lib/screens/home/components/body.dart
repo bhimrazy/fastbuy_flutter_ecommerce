a59a5c3b95c7312d4e3a3f09a48edc8c2bdcf9b5
bhimrazy <bhimrajyadav977@gmail.com>
2020-10-05 18:11:47 +0545

    size config file added

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,8 +1,25 @@
+import 'package:fastbuy_flutter_ecommerce/size_config.dart';
 import 'package:flutter/material.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: SingleChildScrollView(
+          child: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(),
+            child: Row(children: [
+              Container(
+                width: SizeConfig.screenWidth * 0.5,
+                height: 50,
+                color: Colors.black,
+              )
+            ]),
+          )
+        ],
+      )),
+    );
   }
 }
