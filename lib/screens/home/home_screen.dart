63b550bb368d7ef8a4646f01946a0fee57272f12
bhimrazy <bhimrajyadav977@gmail.com>
2020-10-05 17:31:26 +0545

    homescreen and its body code setup

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,1 +1,11 @@
+import 'package:fastbuy_flutter_ecommerce/screens/home/components/body.dart';
+import 'package:flutter/material.dart';
 
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
