e8155c43ee1c79fc3f124cb2b765e330f07e83e9
bhimrazy <bhimrajyadav977@gmail.com>
2020-10-05 18:27:23 +0545

    constants file added and home screen appbar update going on

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,5 +1,7 @@
+import 'package:fastbuy_flutter_ecommerce/constants.dart';
 import 'package:fastbuy_flutter_ecommerce/size_config.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Body extends StatelessWidget {
   @override
@@ -9,14 +11,45 @@ class Body extends StatelessWidget {
           child: Column(
         children: [
           Padding(
-            padding: EdgeInsets.symmetric(),
-            child: Row(children: [
-              Container(
-                width: SizeConfig.screenWidth * 0.5,
-                height: 50,
-                color: Colors.black,
-              )
-            ]),
+            padding: EdgeInsets.symmetric(
+                horizontal: getProportionateScreenWidth(20)),
+            child: Row(
+              children: [
+                Container(
+                  width: SizeConfig.screenWidth * 0.6,
+                  height: 50,
+                  color: Colors.black,
+                  decoration: BoxDecoration(
+                    color: kSecondaryColor.withOpacity(0.1),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: TextField(
+                    onChanged: (value) {
+                      //search value
+                    },
+                    decoration: InputDecoration(
+                        enabledBorder: InputBorder.none,
+                        focusedBorder: InputBorder.none,
+                        hintText: "Search Product",
+                        prefixIcon: Icon(Icons.search),
+                        contentPadding: EdgeInsets.symmetric(
+                          horizontal: getProportionateScreenWidth(20),
+                          vertical: getProportionateScreenWidth(9),
+                        )),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(getProportionateScreenWidth(12)),
+                  height: getProportionateScreenWidth(46),
+                  width: getProportionateScreenWidth(46),
+                  decoration: BoxDecoration(
+                    color: kSecondaryColor.withOpacity(0.1),
+                    shape: BoxShape.circle,
+                  ),
+                  child: SvgPicture.asset("assets/icons/Bell.svg"),
+                )
+              ],
+            ),
           )
         ],
       )),
